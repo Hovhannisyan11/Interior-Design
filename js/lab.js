3d0563b869391f09d92117dfb976186a2f1e94e4
Hovhannisyan11 <garik.hovhannisyan.11@mail.ru>
2023-06-29 19:37:06 +0400

    first commit

---
+++ js/lab.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $('#bar').click(function (){
+    //$('.nav_2').css('display','block')
+    $('.nav_2').show();
+    //$('main').css('backgroundColor','#7F7F7F');
+    //$('main').css('opacity','0.8')
+  })
+})
+  
+
+$(document).ready(function (){
+  $('#a_j').click(function(){
+    //$('.nav_2').css('display','none')
+    $('.nav_2').hide();
+  });
+
+  $('#l_1').click(function(){
+    $('.nav_2').hide();
+  });
+  $('#l_2').click(function(){
+    $('.nav_2').hide();
+  });
+
+  $('#l_3').click(function(){
+    $('.nav_2').hide();
+  });
+
+  $('#l_4').click(function(){
+    $('.nav_2').hide();
+  });
+
+  $('#l_5').click(function(){
+    $('.nav_2').hide();
+  });
+
+  $('#l_6').click(function(){
+    $('.nav_2').hide();
+  })
+
+})
